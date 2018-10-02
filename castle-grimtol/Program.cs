07669a353d9645aa21ec820eac8feee328248891
christensenjoshua <christensen.joshua@outlook.com>
2018-10-01 20:13:22 -0600

    First stages.

--- castle-grimtol/Program.cs
+++ castle-grimtol/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using grimtol_lib;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,28 @@ namespace castle_grimtol
     {
         static void Main(string[] args)
         {
+            var _game = new Gameplay();
+            Console.WriteLine("Welcome to Castle Grimtol.  Would you like to enter? (Y/N)");
+            string resp = Console.ReadLine();
+            resp.ToUpper();
+            if (resp == "Y")
+            {
+                bool playing = true;
+                Console.WriteLine(_game.StartGame());
+                while (playing)
+                {
+                    Console.Clear();
+                    Console.WriteLine("What do you do?");
+                    string cmd = Console.ReadLine();
+                    Console.WriteLine(_game.ProcessCommand(cmd));
+                    Console.ReadLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Fair enough, goodbye!");
+                Console.ReadLine();
+            }
         }
     }
 }
